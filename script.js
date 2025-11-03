4084ec4b52cec4c95fa32464536952d14d0b1aa5
Mahrozfatima786 <mahrozfatima156@gmail.com>
2025-11-03 23:19:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
-//your JS code here. If required.
 const form = document.getElementById("votingForm");
 
-    form.addEventListener("submit", function(event) {
-      event.preventDefault();
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
 
-      const name = document.getElementById("name").value.trim();
-      const age = document.getElementById("age").value.trim();
+  const name = document.getElementById("name").value.trim();
+  const age = document.getElementById("age").value.trim();
 
-      // Validation
-      if (!name || !age) {
-        alert("Please enter valid details.");
-        return;
+  if (!name || !age) {
+    alert("Please enter valid details");
+    return;
+  }
+
+  const checkAge = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (parseInt(age) > 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${name}. You aren't old enough.`);
       }
+    }, 4000);
+  });
 
-      // Creating a Promise
-      const checkAge = new Promise((resolve, reject) => {
-        setTimeout(() => {
-          if (parseInt(age) > 18) {
-            resolve(`Welcome, ${name}. You can vote.`);
-          } else {
-            reject(`Oh sorry ${name}. You aren't old enough.`);
-          }
-        }, 4000);
-      });
-
-      // Handling the Promise
-      checkAge
-        .then((message) => alert(message))
-        .catch((error) => alert(error));
-    });
+  checkAge
+    .then((message) => alert(message))
+    .catch((error) => alert(error));
+});
